2a635ea7aee0cad37cbe7d8b9c7f2b578ea300e3
wangfp <wangfp_nk@163.com>
2017-10-08 17:01:34 +0800

    '.'

--- CS_study/settings.py
+++ CS_study/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
 STATIC_ROOT = STATIC_DIR
 
